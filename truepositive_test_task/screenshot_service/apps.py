46557c51d1a058e21fedb85a727d3fe8b8450c99
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-04-26 19:10:32 +0300

    init commit

---
+++ truepositive_test_task/screenshot_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScreenshotServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'truepositive_test_task.screenshot_service'
