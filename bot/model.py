8807691fc202faa6662e8ab64e6dc14a992211eb
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-04-30 19:48:02 +0300

    - изменение поля user на user_id - изменение user на user_id при инициализации

--- bot/model.py
+++ bot/model.py
@@ -1,7 +1,7 @@
 import json
 
 from pydantic import BaseModel
-from typing import List, Optional, Dict, Any
+from typing import Optional, Dict
 from datetime import datetime
 
 
@@ -17,15 +17,21 @@ class Screenshot(BaseModel):
     """
     Класс для репрезентации ответа Screenshot c API
     """
-    user: int
+    user_id: int
     image: str
-    uploaded_at: Optional[datetime]
+    uploaded_at: Optional[str]
     url: str
     whois: Dict
+    message_id: int
 
     def __init__(self, **data):
         data['whois'] = json.loads(data["whois"])
-        if data.get("user_id", None):
-            data["user"] = data["user_id"]
-            del data["user_id"]
+        if data.get("user", None):
+            data["user_id"] = data["user"]
+            del data["user"]
         super().__init__(**data)
+
+
+a = ['user_id', 'image', 'uploaded_at', 'url', 'whois', 'message_id']
+b = ['user_id', 'image', 'uploaded_at', 'url', 'whois', 'message_id']
+print( a == b)
