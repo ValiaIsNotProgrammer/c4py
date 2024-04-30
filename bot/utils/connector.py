e73cadde69cd3250f99be04d04687a810c25bbf9
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-04-30 19:44:39 +0300

    - рефакторинг BaseConnector - исправление бага с неправильным добавления "/media" - исправления вывода запросов через логгер

--- bot/utils/connector.py
+++ bot/utils/connector.py
@@ -1,7 +1,6 @@
 from typing import Union, List
 
 import aiohttp
-from aiohttp import ClientResponse
 from loguru import logger
 
 from bot.config import BACKEND_URL
@@ -13,15 +12,15 @@ from bot.utils.extra import timer
 def log_request_data(func):
     async def wrapper(*args, **kwargs):
         endpoint = args[1]
-        model = args[2] if len(args) > 2 else None
-        data = kwargs.get('model') if kwargs.get('model') else {}
+        model = kwargs
 
         logger.info(f"Calling {func.__name__} method with:")
         logger.info(f"Endpoint: {endpoint}")
         logger.info(f"Model: {model}")
-        logger.info(f"Data: {data}")
 
-        return await func(*args, **kwargs)
+        result = await func(*args, **kwargs)
+        logger.info(f"Method {func.__name__} /{endpoint} return result: {result}")
+        return result
 
     return wrapper
 
@@ -30,84 +29,42 @@ class BaseConnector:
 
     def __init__(self, backend_url: str = BACKEND_URL):
         self.backend_url = backend_url
-        self.model_endpoints = {
-            "users": BotUser,
-            "screenshots": Screenshot,
-        }
-        logger.info("Connector initialized. BackendUrl: {} Endpoints: {}".format(self.backend_url, self.model_endpoints))
+        logger.info("Connector initialized. BackendUrl: {}".format(self.backend_url))
 
     @log_request_data
-    async def get(self, endpoint: str, model: Union[BotUser, Screenshot] = None, is_list=False) \
-            -> Union[BotUser, Screenshot, None, List[BotUser], List[BotUser]]:
+    async def get(self, endpoint: str, model: Union[BotUser, Screenshot] = None) -> dict:
         params = self.get_dict_from(model) if model else {}
         url = self.get_full_url(endpoint)
         async with aiohttp.ClientSession(headers={'Content-Type': 'application/json'}) as session:
             async with session.get(url, params=params) as response:
-                if is_list:
-                    return await self.models_or_nan_from(response)
-                return await self.model_or_nan_from(response)
+                return await response.json()
 
     @log_request_data
-    async def post(self, endpoint: str, model: Union[BotUser, Screenshot] = None, **kwargs) \
-            -> Union[BotUser, Screenshot, None]:
+    async def post(self, endpoint: str, model: Union[BotUser, Screenshot] = None, **kwargs) -> dict:
         data = self.get_dict_from(model) if model else {}
         data = data | kwargs
         url = self.get_full_url(endpoint)
-        async with aiohttp.ClientSession(headers={'Content-Type': 'application/json', "accept": "application/json"}) as session:
+        async with aiohttp.ClientSession(
+                headers={'Content-Type': 'application/json', "accept": "application/json"}) as session:
             async with session.post(url, json=data) as response:
-                return await self.model_or_nan_from(response)
+                return await response.json()
 
     @log_request_data
-    async def put(self, endpoint: str, model: Union[BotUser, Screenshot]) -> Union[BotUser, None]:
+    async def put(self, endpoint: str, model: Union[BotUser, Screenshot]) -> dict:
         data = self.get_dict_from(model)
         url = self.get_full_url(endpoint)
         async with aiohttp.ClientSession(headers={'Content-Type': 'application/json'}) as session:
             async with session.put(url, json=data) as response:
-                return await self.model_or_nan_from(response)
+                return await response.json()
 
-    async def model_or_nan_from(self, response: ClientResponse) -> Union[BotUser, Screenshot, None]:
-
-        endpoint = response.real_url.path.split("/")[1]
-        logger.trace("Model response from endpoint: {}".format(endpoint))
-        if response.status in [200, 201]:
-            data = await response.json()
-            logger.trace("Model response: {}".format(data))
-            return self.model_endpoints[endpoint](**data)
-        elif response.status == 400:
-            logger.error("Model response from endpoint: {}, error: {}".format(endpoint, response.json()))
-            return None
-        else:
-            logger.error("Model response from endpoint: {}, error: {}".format(endpoint, response.json()))
-            raise ValueError(f'Unexpected response status {response.status}, reason: {response.reason}')
-
-    async def models_or_nan_from(self, response: ClientResponse) -> Union[List[BotUser], List[BotUser], None]:
-        endpoint = response.real_url.path.split("/")[1]
-        logger.trace("Model response from endpoint: {}".format(endpoint))
-        models = []
-        if response.status in [200, 201]:
-            data = await response.json()
-            for model in data:
-                logger.trace("Model response: {}".format(data))
-                models.append(self.model_endpoints[endpoint](**model))
-            return models
-        elif response.status == 400:
-            logger.error("Model response from endpoint: {}, error: {}".format(endpoint, response.json()))
-            return None
-        else:
-            logger.error("Model response from endpoint: {}, error: {}".format(endpoint, response.json()))
-            raise ValueError(f'Unexpected response status {response.status}, reason: {response.reason}')
-
-    def get_full_url(self, url: str) -> str:
-        full_url = f"{self.backend_url}{url}/"
+    def get_full_url(self, endpoint: str) -> str:
+        full_url = f"{self.backend_url}{endpoint}/"
         logger.trace("Full url: {}".format(full_url))
         return full_url
-
 
     @staticmethod
     def get_dict_from(model: Union[BotUser, Screenshot]) -> dict:
         return model.dict()
-
-
 
 
 class APIConnector(BaseConnector):
@@ -118,32 +75,54 @@ class APIConnector(BaseConnector):
         "screenshot": "screenshots",
         "get_screenshot": "screenshots/{}",
     }
+    ENDPOINTS_MODELS = [BotUser, Screenshot]
     logger.info("Connector initialized. BackendUrl: {} Endpoints: {}".format(BACKEND_URL, ENDPOINTS))
+
+    async def model_or_error_from(self, json: dict) -> Union[BotUser, Screenshot, None]:
+        model = self.__get_model_from_data(json)
+        if model:
+            logger.info(f"Model: {model}")
+            return model
+        logger.error(f"Model response error {json}")
+        raise ValueError(f'Unexpected response answer: {json}')
+
+    async def models_or_error_from(self, json: dict) -> Union[List[BotUser], List[Screenshot], None]:
+        models = []
+        for model_dict in json:
+            models.append(self.__get_model_from_data(model_dict))
+        if models:
+            logger.info(f"Models: {models}")
+            return models
+        logger.error(f"Models response error {json}")
+        raise ValueError(f'Unexpected response answer: {json}')
 
     async def get_user(self, user_id: int) -> Union[BotUser, None]:
         endpoint = self.ENDPOINTS["get_user"].format(user_id)
         logger.info("Getting user: {}".format(endpoint))
-        user = await self.get(endpoint)
+        json = await self.get(endpoint)
         logger.success("User was get successfully")
-        return user
+        return await self.model_or_error_from(json)
 
     async def create_user(self, user: BotUser) -> Union[BotUser, None]:
         logger.info("Creating user: {}".format(user))
-        exist_user = await self.get_user(user.id)
-        if exist_user:
-            logger.success("User already exists. Returning user: {}".format(user))
-            return exist_user
+        try:
+            exist_user = await self.get_user(user.id)
+            if exist_user:
+                logger.success("User already exists. Returning user: {}".format(user))
+                return exist_user
+        except ValueError:
+            logger.warning("User does not exist. Creating user: {}".format(user))
         endpoint = self.ENDPOINTS["user"]
-        user = await self.post(endpoint, model=user)
+        json = await self.post(endpoint, model=user)
         logger.success("User was created successfully")
-        return user
+        return await self.model_or_error_from(json)
 
     async def update_user(self, user: BotUser) -> Union[BotUser, None]:
         endpoint = self.ENDPOINTS["user"]
         logger.info("Updating user: {}".format(user))
-        user = await self.put(endpoint, model=user)
+        json = await self.put(endpoint, model=user)
         logger.success("User was updated successfully")
-        return user
+        return await self.model_or_error_from(json)
 
     async def get_language(self, user_id: int) -> str:
         logger.info("Getting language: {}".format(user_id))
@@ -162,26 +141,33 @@ class APIConnector(BaseConnector):
         logger.success("User language was updated successfully")
         return user
 
-    async def get_screenshot(self, url: str) -> Screenshot:
-        "UNUSED"
-        # endpoint = self.ENDPOINTS["get_screenshot"].format(url)
-        return await self.get(url)
+    async def get_screenshot_url_from_message_id(self, user_id: int, message_id: int) -> str:
+        endpoint = self.ENDPOINTS["screenshot"]
+        logger.info("Getting screenshot url from message id: {}".format(message_id))
+        response = await self.get(endpoint)
+        screenshots = await self.models_or_error_from(response)
+        for screenshot in screenshots:
+            if screenshot.message_id == message_id:
+                return screenshot.url
 
     @timer
-    async def create_screenshot(self, user_id: int, url: str, full_url=True, get_time=False) -> Screenshot:
+    async def create_screenshot(self, user_id: int, url: str, message_id: int, get_time=False) -> Union[Screenshot, dict]:
         logger.info("Creating screenshot: {}, user: {}".format(url, user_id))
         endpoint = self.ENDPOINTS["screenshot"]
-        screenshot = await self.post(endpoint, id=user_id, url=url)
-        if full_url:
-            screenshot.image = self.get_full_url(screenshot.image)
-            logger.info("Full screenshot URL: {}".format(screenshot.image))
-        logger.success("Screenshot was created successfully")
-        return screenshot
+        json = await self.post(endpoint, id=user_id, url=url, message_id=message_id)
+        try:
+            screenshot = await self.model_or_error_from(json)
+        except ValueError as ex:
+            logger.error(f"Screenshot response error {json}, error: {ex}")
+            return json
+        logger.success("Screenshot was created successfully {}".format(screenshot))
+        return self.screenshot_with_full_url_field(screenshot)
 
     async def get_whois(self, url: str) -> Union[dict, None]:
         logger.info("Getting whois: {}".format(url))
         endpoint = self.ENDPOINTS["screenshot"]
-        screenshots = await self.get(endpoint, is_list=True)
+        json = await self.get(endpoint)
+        screenshots = await self.models_or_error_from(json)
         logger.success("Whois was retrieved successfully")
         return self.get_whois_from(screenshots, url)
 
@@ -194,6 +180,22 @@ class APIConnector(BaseConnector):
                 return screenshot.whois
         logger.warning("No valid whois was retrieved successfully")
         raise KeyError("No whois found for url: {}".format(url))
+
+    def screenshot_with_full_url_field(self, screenshots: Screenshot) -> Screenshot:
+        logger.info("Get image URL: {}".format(screenshots.image))
+        screenshots.image = self.backend_url + screenshots.image[1:]
+        logger.info("Edt image URL to: {}".format(screenshots.image))
+        return screenshots
+
+    def __get_model_from_data(self, json: dict) -> Union[BotUser, Screenshot, None]:
+        for model in self.ENDPOINTS_MODELS:
+            logger.trace(f"Added model {model}")
+            try:
+                m = model(**json)
+                return m
+            except Exception:
+                pass
+            logger.warning("Model fields do not match json fields: {}".format(json))
 
 
 api_connector = APIConnector()
