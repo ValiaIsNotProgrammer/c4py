09ab4b6720dc47af5cc29077d49c2ac165e26db7
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-04-30 19:50:57 +0300

    - добавлено динамическое изменение имени Screenshot.image через инициализацию сериализатора

--- api/screenshot_service/serializers.py
+++ api/screenshot_service/serializers.py
@@ -1,15 +1,30 @@
+from datetime import datetime
+
 from loguru import logger
 from rest_framework import serializers
 
 from api.screenshot_service.models import Screenshot
-
-
+from api.utils.screenshots import screenshot_maker
 
 
 class ScreenshotSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Screenshot
-        fields = ["image", "uploaded_at", "user", "url", "whois"]
+        fields = ["image", "uploaded_at", "user", "url", "whois", "message_id"]
+
+    def __init__(self, *args, **kwargs):
+        """
+        При инициализации класса мы динамически меняем имя image
+        """
+        super().__init__(*args, **kwargs)
+        try:
+            image = kwargs["data"]["image"]
+            uploaded_at = datetime.now()
+            kwargs["data"]["uploaded_at"] = uploaded_at.strftime("%Y-%m-%d %H:%M:%S")
+            image.name = screenshot_maker.get_name_for_image(kwargs["data"])
+        except KeyError:
+            pass
 
     def create(self, validated_data):
         logger.info("Creating screenshot")
