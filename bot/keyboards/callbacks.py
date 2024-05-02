efb16eec30d3b599f69ad572a07fe071b3a9caf6
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-05-02 23:02:44 +0300

    исправление документации

--- bot/keyboards/callbacks.py
+++ bot/keyboards/callbacks.py
@@ -8,8 +8,9 @@ class InlineCallback(CallbackData, prefix="inline_callback"):
     """
     Класс для передачи метаданных для bot.handlers.keyboard_handlers
 
-    button_name - парамент который передается в аргумент callback_data
-    last_msg_id - парамент, который позволяет сослаться на последнее сообщение связанное с конкретным URL
+    :button_name - парамент который передается в аргумент callback_data
+    :user_msg_id - опциональный аргумент, который используется для получения ссылки, с user.id
+    Т.к. CallbackData позволяет сохраняться данные, длина которых < 64, передавать ссылку мы не можем
 
     """
     button_name: str
