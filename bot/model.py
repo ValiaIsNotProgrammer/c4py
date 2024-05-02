35e33a94575079e2fa951e496d178b1b9bb986db
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-05-02 23:04:55 +0300

    - добавление поля id - добавление документации

--- bot/model.py
+++ bot/model.py
@@ -2,12 +2,11 @@ import json
 
 from pydantic import BaseModel
 from typing import Optional, Dict
-from datetime import datetime
 
 
 class BotUser(BaseModel):
     """
-    Класс для репрезентации ответа User c API
+    Класс BotUser для репрезентации json ответа c API
     """
     id: int
     language: str
@@ -15,7 +14,7 @@ class BotUser(BaseModel):
 
 class Screenshot(BaseModel):
     """
-    Класс для репрезентации ответа Screenshot c API
+    Класс Screenshot для репрезентации json ответа c API
     """
     user_id: int
     image: str
@@ -23,8 +22,10 @@ class Screenshot(BaseModel):
     url: str
     whois: Dict
     message_id: int
+    process_time: float
 
     def __init__(self, **data):
+        "Метод инициализации для переопределния входящих данных пользователя"
         data['whois'] = json.loads(data["whois"])
         if data.get("user", None):
             data["user_id"] = data["user"]
@@ -32,6 +33,16 @@ class Screenshot(BaseModel):
         super().__init__(**data)
 
 
-a = ['user_id', 'image', 'uploaded_at', 'url', 'whois', 'message_id']
-b = ['user_id', 'image', 'uploaded_at', 'url', 'whois', 'message_id']
-print( a == b)
+class Stats(BaseModel):
+    "Класс Stats для репрезентации json ответа c API"
+    all_users_count: Optional[int]
+    all_screenshots_count: Optional[int]
+    average_process_time: Optional[float]
+    average_len_screenshots_user: Optional[float]
+    count_screenshots_on_day: Optional[int]
+
+    def __init__(self, **data):
+        "Метод инициализации для округления average_process_time"
+        data["average_process_time"] = round(data["average_process_time"], 2)
+        super().__init__(**data)
+
