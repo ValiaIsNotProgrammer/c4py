fef1d9483427f644a90633596b3bca5d6b809ca1
valiaisnotprogrammer <ValiaChernykh@yandex.ru>
2024-05-02 23:08:16 +0300

    - рефакторинг - добавление ручки статистики

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls.static import static
-from django.template.defaulttags import url
 from django.urls import path, include
-from django.views.static import serve
-from rest_framework import permissions, routers
+from rest_framework import routers
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from api.users.views import UserViewSet
-from api.screenshot_service.views import ScreenshotViewSet, serve_media, ScreenshotImageViewSet
+from users.views import UserViewSet
+from screenshot_service.views import ScreenshotViewSet
+from stats.views import StatsListView
 
-from api.config.settings import MEDIA_URL, MEDIA_ROOT, DEBUG
+from config.settings import MEDIA_URL, MEDIA_ROOT, DEBUG
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,11 +16,10 @@ schema_view = get_schema_view(
         default_version='v1',
         description="API documentation",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
+        contact=openapi.Contact(email="ValiaChernykh@yandex.ru"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    # permission_classes=(permissions.AllowAny,),
 )
 
 router = routers.DefaultRouter()
@@ -33,13 +31,6 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('stats/', StatsListView.as_view(), name='stats'),
     path('', include(router.urls)),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
-
-# if DEBUG:
-#     urlpatterns += [
-#         url(r'^media/(?P<path>.*)$', serve, {
-#             'document_root': MEDIA_ROOT,
-#         }),
-#     ]
-# print(urlpatterns)
